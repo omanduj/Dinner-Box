35490426b1634b86f9ce0063c2edd6af6dda2cd8
omanduj <omanduj@gmail.com>
2021-12-21 02:06:03 -0600

    error free curl requests

--- food_proj/food_app/users_token/views.py
+++ food_proj/food_app/users_token/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.utils.functional import wraps
 from .models import AuthForm
 from passlib.hash import pbkdf2_sha256
 import jwt
 import datetime
 import pymongo
+
 
 app = {}
 app['SECRET_KEY'] = 'thisisthesecretkey'
@@ -16,10 +18,12 @@ db = (
 )
 
 # Create your views here.
+
+# curl -d "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjpudWxsLCJleHAiOjE2NDAwNzM2NDh9.2zxoyhd3ovA6rBPTRCdKw1OZV-7wQfsiZHvh8teWkNE
 def check_for_token(func):
     @wraps(func)
-    def wrapped(*args, **kwargs):
-        token = request.session.get("token")
+    def wrapped(request, *args, **kwargs):
+        token = request.POST.get('token')
         if not token:
             return JsonResponse({'message': 'Missing Token!'}), 401
         try:
@@ -28,8 +32,6 @@ def check_for_token(func):
             return JsonResponse({'Message': 'Invalid Token'})
         return func(*args, **kwargs)
     return wrapped
-
-
 
 def index(request):
     request.session['logged_in'] = False
@@ -42,7 +44,8 @@ def index(request):
 def public(request):
     return JsonResponse({'Success': "Anyone can view this"})
 
-# @check_for_token
+@check_for_token
+@csrf_exempt
 def auth(request):
     if request.method == 'POST':
         return JsonResponse({'Success': "Only Viewable with a token"})
@@ -55,11 +58,12 @@ def login(request):
         user = db.user_tokens.find_one({"email": request.POST.get("email")})
         if user == None:
             password = pbkdf2_sha256.encrypt(request.POST.get('password'))
-            db.user_tokens.insert_one({"email": request.POST.get("email"), 'password': password})
             request.session['logged_in'] = True
             token = jwt.encode({'user': request.POST.get('username'),
                                 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)},
                                 app['SECRET_KEY'])
+            db.user_tokens.insert_one({"email": request.POST.get("email"), 'password': password, 'token': {str(datetime.datetime.utcnow()): token}})
+
             return JsonResponse({'Success': 'You have been Registered', 'token': token})
 
         elif user != None:
@@ -68,8 +72,10 @@ def login(request):
                 token = jwt.encode({'user': request.POST.get('username'),
                                     'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)},
                                     app['SECRET_KEY'])
-                return JsonResponse({'token': token})
+                db.user_tokens.update({"email": request.POST.get("email")}, {'$set': {'token.{}'.format(datetime.datetime.utcnow()): token}})
+                print(datetime.datetime.utcnow() + datetime.timedelta(seconds=30))
+                return JsonResponse({'New Token Created': token})
             else:
-                return JsonResponse({'Error': 'Email or Password Not Found'})
+                return JsonResponse({'Error': 'Email Does Not Corresponds to Password'})
         else:
             return  JsonResponse({'Error': "Not Found"})
