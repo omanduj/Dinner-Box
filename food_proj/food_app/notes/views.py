bf9687cacb939ab5bf1137c4f98e365c112e77a6
omanduj <omanduj@gmail.com>
2022-01-12 19:13:27 -0600

    For future ordering of notes

--- food_proj/food_app/notes/views.py
+++ food_proj/food_app/notes/views.py
@@ -1,9 +1,61 @@
 from django.shortcuts import render
-from users.db_operations import add_note, check_note_exists, get_user_notes
+from users.db_operations import add_note, check_note_exists, get_user_notes, get_one_user, delete_note
 from django.http import JsonResponse
-from users.db_operations import get_one_user, delete_note
+import collections
+# from users.db_operations import get_one_user, delete_note, get_user_notes
+
 
 # Create your views here.
+def order_notes_name(request):            #have this and next switch between reverse depending on user selection (form, ect)
+    notes = get_user_notes(request.session["user"]['email'])
+    notes = list(notes)[0]['Notes']                                  #used to format returned information into a dict
+    # notes = dict(sorted(notes.items(), reverse=True))
+    notes = dict(sorted(notes.items()))                              #used to sort notes based on name of restaurant
+    standard_note = {'Notes': notes}
+    # print(standard_note)
+    return standard_note
+
+def order_notes_rating(request):
+    notes = get_user_notes(request.session["user"]['email'])
+    notes = list(notes)[0]['Notes']
+    notes_order = {}
+    for key, value in notes.items():
+        notes_order[key] = value['rating']
+    # notes_order = dict(sorted(notes_order.items(), key=lambda item: item[1], reverse=True))
+    notes_order = dict(sorted(notes_order.items(), key=lambda item: item[1]))
+    for key, value in notes.items():
+        notes_order[key] = value
+    standard_note = {'Notes': notes_order}
+    del notes_order
+    # print(standard_note)
+    return standard_note
+
+
+def order_notes_date(request):
+    notes = get_user_notes(request.session["user"]['email'])
+    notes = list(notes)[0]['Notes']
+    notes_order = {}
+    for key, value in notes.items():
+        notes_order[key] = value['date']
+    # notes_order = dict(sorted(notes_order.items(), key=lambda item: item[1], reverse=True))
+    notes_order = dict(sorted(notes_order.items(), key=lambda item: item[1]))
+    for key, value in notes.items():
+        notes_order[key] = value
+    standard_note = {'Notes': notes_order}
+    del notes_order
+    # print(standard_note)
+    return standard_note
+
+
+
+
+def order_notes(request):
+    ordering = request.POST.get('ordering')
+    return JsonResponse({'Ordering': ordering})
+
+
+
+
 
 def create_note(request):
     if request.method == "GET":
@@ -25,21 +77,41 @@ def create_note(request):
 
 def view_notes(request):
     if request.method == "GET":
-        note_collection = []
+        # note_collection = []
         email = request.session["user"]['email']
         user_notes = get_one_user(email)
         del user_notes['_id']
         del user_notes['email']
         del user_notes['password']
-        if len(user_notes['Notes']) > 0:
-            return render(request, 'note_viewer.html', {'notes': user_notes})
+        try:
+            if len(user_notes['Notes']) > 0:
+                # user_notes = order_notes_name(request)
+                # user_notes = order_notes_rating(request)
+                # user_notes = order_notes_date(request)
+                return render(request, 'note_viewer.html', {'notes': user_notes})
+        except Exception as e:
+            return render(request, 'note_viewer.html', {'error': 'Add Some Notes!'})
         return render(request, 'note_viewer.html', {'error': 'Add Some Notes!'})
+
+
+    # if request.method == "POST":
+    #     ordering = request.POST.get('ordering')
+    #     print(ordering)
+    #     if ordering == 'name':
+    #         user_notes = order_notes_name(request)
+    #     if ordering == 'rating':
+    #         user_notes = order_notes_rating(request)
+    #         user_notes['Notes'][-1] = 'AAA'
+    #         print(user_notes)
+    #     if ordering == 'date':
+    #         user_notes = order_notes_date(request)
+    #     return JsonResponse({'Success': user_notes})
 
 def delete_user_note(request):
     restaurant_name = request.POST.get('restaurant_name')
     email = request.session["user"]['email']
     notes_found = delete_note(email, restaurant_name)
-    print(notes_found, 'GG')
+    # print(notes_found, 'GG')
     if notes_found:
         return JsonResponse({'Success': notes_found})
     return JsonResponse({'Error': 'Not Foundsss'})
