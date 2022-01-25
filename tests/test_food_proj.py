82bfda463aa0f8ca90344aafa9cdd1c355edfa35
omanduj <omanduj@gmail.com>
2022-01-25 00:06:25 -0600

    Edits to readme as to how to run tests

--- tests/test_food_proj.py
+++ tests/test_food_proj.py
@@ -1,5 +1,7 @@
 from food_proj import __version__
 
+#TEST CAN BE RUN BY TYPING THE FOLLOWING COMMAND IN THE TERMINAL:
+#python manage.py test
 
 def test_version():
     assert __version__ == '0.1.0'
